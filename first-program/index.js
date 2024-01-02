fdd52993944ff0d2c71b4bf95558814f50d12d77
Tanvir Rudro <rudro987@gmail.com>
2024-01-02 16:05:28 +0600

    add function using type declaration

---
+++ first-program/index.js
@@ -0,0 +1,5 @@
+var addNumber = function (num1, num2) {
+    return num1 + num2;
+};
+// console.log(addNumber(20,"30"));
+console.log(addNumber(20, 30));
---
+++ first-program/index.ts
@@ -0,0 +1,7 @@
+const addNumber = (num1: number, num2: number) => {
+    return num1 + num2;
+}
+
+// console.log(addNumber(20,"30"));
+console.log(addNumber(20, 30));
+

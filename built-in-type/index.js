14cf95a2bd2748cdc5aca6787d2bf8a316398f3a
Tanvir Rudro <rudro987@gmail.com>
2024-01-02 21:41:28 +0600

    built in data types

---
+++ built-in-type/index.js
@@ -0,0 +1,14 @@
+// built in types in typescript: numebr, string, boolean, undefined, null, void
+var userId;
+var firstName;
+var lastName;
+var isActivated;
+userId = 101;
+firstName = "Md Tanvir Hossain";
+lastName = "Talukder";
+isActivated = true;
+console.log(userId, firstName, lastName, isActivated);
+var display = function () {
+    console.log("Hi I am display");
+};
+display();
---
+++ built-in-type/index.ts
@@ -0,0 +1,19 @@
+// built in types in typescript: numebr, string, boolean, undefined, null, void
+
+let userId: number;
+let firstName: string;
+let lastName: string;
+let isActivated: boolean;
+
+userId = 101;
+firstName = "Md Tanvir Hossain";
+lastName = "Talukder";
+isActivated = true;
+
+console.log(userId, firstName, lastName, isActivated);
+
+const display = () : void => {
+    console.log("Hi I am display");
+    
+}
+display();

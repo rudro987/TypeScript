c55adc1662cfa589e0654f0bc437783c05ac78de
Tanvir Rudro <rudro987@gmail.com>
2024-01-03 01:28:27 +0600

    inheritence type

--- class-typescript/class-demo.ts
+++ class-typescript/class-demo.ts
@@ -1,4 +1,4 @@
-class User{
+class User {
     // properties, methods, constructor
     userName: string;
     age: number;
---
+++ class-typescript/inheritence-demo.ts
@@ -0,0 +1,33 @@
+class User {
+    userName: string;
+    age: number;
+
+    constructor(userName: string, age: number){
+        this.userName = userName;
+        this.age = age;
+    }
+    display(): void {
+        console.log(`username: ${this.userName}, age: ${this.age}`);
+        
+    }
+};
+
+class Student extends User {
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number){
+        super(userName, age);
+        this.studentId = studentId;
+    }
+    display(): void {
+        console.log(`username: ${this.userName}, age: ${this.age}, id: ${this.studentId}`);    
+    }
+};
+
+let student1 = new Student("John", 30, 1302523);
+student1.display();
+
+// let user1 = new User("John", 30);
+// user1.display();
+
+// class object
